1bd75ecb8f971b09d30d37f3bbeaef3b8b251a2a
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-10-15 22:34:39 -0400

    succesfully spawned grid

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
 const gridContainer = document.querySelector("#grid-container"); 
-let column;
-let row;
-
 let rowNum = 16; 
 let colNum = 16;
 
-const grid = rowNum * colNum; 
 function spawnGrid(rowNum, colNum) {
-
-    // for (let i = 0; i < grid; i++) {
-    //     let spawn = document.createElement("div"); 
-    //     spawn.style.backgroundColor = "white"; 
-    //     spawn.style.border = "1px solid hotpink"; 
-    //     spawn.style.width = "40px";
-    //     spawn.style.height = "30px"; 
-    //     gridContainer.appendChild(spawn); 
-    // } 
-
-    for (let i = 0; i < grid; i++) {
-        spawnCol(colNum);
-        spawnRow(rowNum);
-    }
-}
-spawnGrid(rowNum, colNum);
-console.log(grid);
-
-function spawnCol(colNum) {
-
     for (let i = 0; i < colNum; i++) {
-        let column = document.createElement("div");
-        column.style.backgroundColor = "white";
-        column.classList.add("column");
-        gridContainer.appendChild(column); 
+        spawnCol(rowNum);
     }
 }
-console.log(colNum);
 
-function spawnRow(rowNum) {
+function spawnCol(Num) {
+    let column = document.createElement("div");
+    column.style.backgroundColor = "white";
+    column.classList.add("column");
+
     for (i = 0; i < rowNum; i++) {
         let row = document.createElement('div');
         row.style.backgroundColor = "white";
         row.classList.add("row");
         column.appendChild(row); 
     }
+    gridContainer.appendChild(column); 
 }
-console.log(rowNum); 
+
+spawnGrid(rowNum, colNum);
+
+
+//TIPS BY PHOSPHOR: 
+/*I also used flex wrap on mine. the workaround I used for wrap and sizes was to calculate and set the width each time I changed the dimensions.
+
+so your issue isn't that column doesn't exist or isn't available. however, look at what column is referring to: the unattached element you just created. you then attach it to the gridContainer. then in your original snippet, you attached the row elements to the unattached element, not the DOM node.
+here's another hint if that's still not clear: think about when you want to call spawnRow so that after appending the divs to the column, the whole thing gets appended to the DOM */ 
+/*something that might make things easier is explicitly passing references to elements as arguments. that takes a lot of the guesswork out of "what was the value of this variable when I called it" and makes troubleshooting easier.*/
+
+//TIPS BY SOFFY 
+/*I don't know if this helps, but when I did the project, I used percentage dimensions, since I knew that each square's size was just (100/the number of squares in a side)%. then it was just a matter of using flex wrap, since the container's size doesn't change */
+
