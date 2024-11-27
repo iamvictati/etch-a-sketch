d966cd2d4c9914d317e5e3c39b38f05eebcf78d7
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-11-27 19:03:26 -0400

    Fixed modal bug + cleaning service

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ function selectGrid() {
             rowNum = gridInput.value; 
             cellNum = gridInput.value; 
             newGrid(rowNum); 
+            document.querySelector('.grid-modal').style.display = 'none'; 
+            document.body.removeChild(gridModal); 
         }
     });
 }
@@ -74,11 +76,9 @@ function  newGrid() {
     if (rowNum >= 1 & rowNum <= 80 ) {
         gridContainer.replaceChildren(); 
         spawnGrid(rowNum); 
-        document.querySelector('.grid-modal').style.display = 'none'; 
-        document.body.removeChild(gridModal); 
     } else {
         alert('Your number is over the limit'); 
-    }
+    } 
 }
 
 function clearGrid() {
@@ -88,19 +88,5 @@ function clearGrid() {
     });
 }
 
-// [1] Eliminate bug with modal function after a second selectiong of grid number 
+// [💜] Eliminate bug with modal function after a second selectiong of grid number 
 // [2] Cteate the pen color select function or event Listeners 
-
-//TIPS BY PHOSPHOR: 
-/*I also used flex wrap on mine. the workaround I used for wrap and sizes was to calculate and set the width each time I changed the dimensions.
-
-so your issue isn't that column doesn't exist or isn't available. however, look at what column is referring to: the unattached element you just created. you then attach it to the gridContainer. then in your original snippet, you attached the row elements to the unattached element, not the DOM node.
-here's another hint if that's still not clear: think about when you want to call spawnRow so that after appending the divs to the column, the whole thing gets appended to the DOM */ 
-//
-/*something that might make things easier is explicitly passing references to elements as arguments. that takes a lot of the guesswork out of "what was the value of this variable when I called it" and makes troubleshooting easier.*/
-
-//TIPS BY SOFFY 
-/*I don't know if this helps, but when I did the project, I used percentage dimensions, since I knew that each square's size was just (100/the number of squares in a side)%. then it was just a matter of using flex wrap, since the container's size doesn't change */
-
-//Check aspect-ratio property 
-
